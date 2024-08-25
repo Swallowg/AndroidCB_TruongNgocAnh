18b050afb50c883fda10f20627d9fdeea19cc113
NgocAnh <lananhsuri012@gmail.com>
2024-08-25 22:04:29 +0700

    MyFirstApplication

---
+++ app/src/main/java/com/example/myfirstapplication/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.myfirstapplication;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView tv= new TextView(this);
+        tv.setText("Hello World!!!");
+        setContentView(tv);
+        setTitle("The first Android program");
+    }
+}
